47924f9be8ac92b8eb178763a28cdf6b8ff3acbc
Ramya2697 <gramyarajan@gmail.com>
2022-01-29 11:40:32 +0530

    new

---
+++ src/main/java/com/mindtree/simplecalculator/Calculate.java
@@ -0,0 +1,19 @@
+package com.mindtree.simplecalculator;
+
+public class Calculate {
+	public static int add(int x, int y)
+	{
+		return x+y;
+	}
+	public static int subtract(int x, int y)
+	{
+		return x-y;
+	}
+	public static void main(String[] args) {
+		int x=100;
+		int y=50;
+		System.out.println(add(x,y));
+		System.out.println(subtract(x,y));
+	}
+
+}
